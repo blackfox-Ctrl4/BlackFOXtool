16426839e1e8d6d63a09a7670952698f30870d33
blackfox-Ctrl4 <72064987+blackfox-Ctrl4@users.noreply.github.com>
2020-09-29 10:09:27 +0330

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,21 @@
+echo "starting downloading python 3 packeges...."
+pip3 install bs4
+echo "--------"
+echo "bs4 Done"
+echo "--------"
+pip3 install requests
+echo "-------------"
+echo "requests Done"
+echo "-------------"
+pip3 install socket
+echo "-----------"
+echo "socket Done"
+echo "-----------"
+pip3 install warnings
+echo "------------"
+echo "warnings Done"
+echo "------------"
+pip3 install ipwhois
+echo "------------"
+echo "ipwhois Done"
+echo "------------"
