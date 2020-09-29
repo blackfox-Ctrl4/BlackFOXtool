cf15a90fe3cbf08953d820f71e9633c7e9d78f04
BlackFox <72064987+blackfox-Ctrl4@users.noreply.github.com>
2020-09-29 21:06:09 +0330

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,6 @@
+
+read -p "install BlackFox ? [y/n]:" yn
+			case $yn in
+			    [Yy]*) pip3 install bs4 ; pip3 install requests ; pip3 install ipwhois ; echo "[✔] Installing ..."; clear ; python3 blackfox.py ;;
+			    [Nn]*) echo "exit ... " ; exit 1 ;;
+			esac
